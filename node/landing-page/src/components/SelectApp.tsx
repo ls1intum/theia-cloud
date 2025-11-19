086cf8c07cbc107c328457feaa815faf9fdeb24e
CodeByNikolas <Nikolas.Hack@tum.de>
2025-11-19 10:57:25 +0100

    again fix "java 17"

--- node/landing-page/src/components/SelectApp.tsx
+++ node/landing-page/src/components/SelectApp.tsx
@@ -4,25 +4,26 @@ interface SelectAppProps {
   appDefinitions: AppDefinition[] | undefined;
   onStartSession: (appDefinition: string) => void;
 }
-export const SelectApp: React.FC<SelectAppProps> = ({
-  appDefinitions,
-  onStartSession
-}: SelectAppProps) => {
+export const SelectApp: React.FC<SelectAppProps> = ({ appDefinitions, onStartSession }: SelectAppProps) => {
   return (
     <div className='App__grid'>
-      {appDefinitions && appDefinitions.map((app, index) => (
-        <button
-          key={index}
-          className='App__grid-item'
-          onClick={() => onStartSession(app.appId)}
-          data-testid={`launch-app-${app.appId}`}
-        >
-          <img src={`/assets/logos/${app.appName.toLowerCase()}-logo.png`} alt={`${app.appName} logo`} className='App__grid-item-logo' />
-          <div className='App__grid-item-launch'>Launch</div>
-          <div className='App__grid-item-text'>{app.appName}</div>
-        </button>
-      ))}
+      {appDefinitions &&
+        appDefinitions.map((app, index) => (
+          <button
+            key={index}
+            className='App__grid-item'
+            onClick={() => onStartSession(app.appId)}
+            data-testid={`launch-app-${app.appId}`}
+          >
+            <img
+              src={`/assets/logos/${app.appName.toLowerCase().replace(/\s+/g, '-')}-logo.png`}
+              alt={`${app.appName} logo`}
+              className='App__grid-item-logo'
+            />
+            <div className='App__grid-item-launch'>Launch</div>
+            <div className='App__grid-item-text'>{app.appName}</div>
+          </button>
+        ))}
     </div>
   );
 };
-

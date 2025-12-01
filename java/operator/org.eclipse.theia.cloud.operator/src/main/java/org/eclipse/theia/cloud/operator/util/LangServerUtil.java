1b8f61ac8011bb9cb919b4fe6503350e506694dc
CodeByNikolas <Nikolas.hack@yahoo.com>
2025-12-01 13:52:50 +0100

    add logging

--- java/operator/org.eclipse.theia.cloud.operator/src/main/java/org/eclipse/theia/cloud/operator/util/LangServerUtil.java
+++ java/operator/org.eclipse.theia.cloud.operator/src/main/java/org/eclipse/theia/cloud/operator/util/LangServerUtil.java
@@ -24,6 +24,7 @@ public class LangServerUtil {
 
     public static void createAndApplyLSService(NamespacedKubernetesClient client, String namespace, String correlationId,
             String sessionResourceName, String sessionResourceUID, AppDefinition appDefinition) {
+        LOGGER.info(formatLogMessage(correlationId, "Creating LS service for session " + sessionResourceName));
         Map<String, String> replacements = new HashMap<>();
         replacements.put("placeholder-servicename", "ls-" + sessionResourceName);
         replacements.put("placeholder-app", "ls-" + sessionResourceName);
@@ -45,11 +46,13 @@ public class LangServerUtil {
         K8sUtil.loadAndCreateServiceWithOwnerReference(client, namespace, correlationId, serviceYaml,
                 "theia.cloud/v1beta9", "Session", sessionResourceName, sessionResourceUID, 0,
                 Collections.emptyMap());
+        LOGGER.info(formatLogMessage(correlationId, "Successfully created LS service for session " + sessionResourceName));
     }
 
     public static void createAndApplyLSDeployment(NamespacedKubernetesClient client, String namespace, String correlationId,
             String sessionResourceName, String sessionResourceUID, AppDefinition appDefinition,
             String lsImage) {
+        LOGGER.info(formatLogMessage(correlationId, "Creating LS deployment for session " + sessionResourceName));
         Map<String, String> replacements = new HashMap<>();
         replacements.put("placeholder-depname", "ls-" + sessionResourceName);
         replacements.put("placeholder-app", "ls-" + sessionResourceName);
@@ -74,9 +77,11 @@ public class LangServerUtil {
                 Collections.emptyMap(), deployment -> {
                     // No extra processing needed for now
                 });
+        LOGGER.info(formatLogMessage(correlationId, "Successfully created LS deployment for session " + sessionResourceName));
     }
 
     public static void updateTheiaDeploymentWithLangServerEnvVars(io.fabric8.kubernetes.api.model.apps.Deployment deployment, String sessionResourceName, String lsImage, AppDefinition appDefinition) {
+        LOGGER.info(formatLogMessage("N/A", "Updating Theia deployment with LS env vars for session " + sessionResourceName));
         String lsServiceName = "ls-" + sessionResourceName;
         String lsHost = lsServiceName;
         LangServerDetails lsDetails = getLangServerDetails(lsImage, appDefinition);
@@ -90,9 +95,11 @@ public class LangServerUtil {
         envVars.add(new EnvVarBuilder().withName(lsDetails.getEnvHostKey()).withValue(lsHost).build());
         envVars.add(
                 new EnvVarBuilder().withName(lsDetails.getEnvPortKey()).withValue(lsDetails.getPort()).build());
+        LOGGER.info(formatLogMessage("N/A", "Successfully updated Theia deployment with LS env vars for session " + sessionResourceName));
     }
 
     public static void deleteLangServerResources(NamespacedKubernetesClient client, String sessionResourceName, String correlationId) {
+        LOGGER.info(formatLogMessage(correlationId, "Deleting LS resources for session " + sessionResourceName));
         String lsResourceName = "ls-" + sessionResourceName;
         try {
             client.services().withName(lsResourceName).delete();

8264ff800e5f19d1bd2bc1f0e241dea853dfbc72
= <kevingruber01@protonmail.com>
2026-02-07 16:32:57 +0100

    fix conflict

--- java/operator/org.eclipse.theia.cloud.operator/src/main/java/org/eclipse/theia/cloud/operator/replacements/DefaultDeploymentTemplateReplacements.java
+++ java/operator/org.eclipse.theia.cloud.operator/src/main/java/org/eclipse/theia/cloud/operator/replacements/DefaultDeploymentTemplateReplacements.java
@@ -72,6 +72,11 @@ public class DefaultDeploymentTemplateReplacements implements DeploymentTemplate
     public static final String PLACEHOLDER_ENV_CACHE_URL = "placeholder-cache-url";
     public static final String PLACEHOLDER_ENV_CACHE_PUSH = "placeholder-cache-push";
 
+    public static final String PLACEHOLDER_CA_BUNDLE_PEM_PATH = "placeholder-ca-bundle-pem-path";
+    public static final String PLACEHOLDER_GRADLE_TRUST_OPTS = "placeholder-gradle-trust-opts";
+    public static final String PLACEHOLDER_TRUST_BUNDLE_MOUNT_PATH = "placeholder-trust-bundle-mount-path";
+    public static final String PLACEHOLDER_TRUST_BUNDLE_CONFIGMAP = "placeholder-trust-bundle-configmap";
+
     protected static final String DEFAULT_UID = "1000";
 
     @Inject
@@ -163,6 +168,21 @@ public class DefaultDeploymentTemplateReplacements implements DeploymentTemplate
             environmentVariables.put(PLACEHOLDER_ENV_CACHE_ENABLED, "false");
             environmentVariables.put(PLACEHOLDER_ENV_CACHE_URL, "");
             environmentVariables.put(PLACEHOLDER_ENV_CACHE_PUSH, "false");
+        }
+
+        if (arguments.isEnableCaching() && arguments.getCacheUrl() != null
+                && arguments.getCacheUrl().startsWith("https")) {
+            // Only set trust config when using HTTPS for the cache
+            environmentVariables.put(PLACEHOLDER_CA_BUNDLE_PEM_PATH, "/etc/ssl/theia-trust/trust-bundle.pem");
+            environmentVariables.put(PLACEHOLDER_GRADLE_TRUST_OPTS,
+                    "-Djavax.net.ssl.trustStore=/etc/ssl/theia-trust/truststore.jks -Djavax.net.ssl.trustStorePassword=changeit");
+            environmentVariables.put(PLACEHOLDER_TRUST_BUNDLE_MOUNT_PATH, "/etc/ssl/theia-trust");
+            environmentVariables.put(PLACEHOLDER_TRUST_BUNDLE_CONFIGMAP, "theia-internal-trust");
+        } else {
+            environmentVariables.put(PLACEHOLDER_CA_BUNDLE_PEM_PATH, "");
+            environmentVariables.put(PLACEHOLDER_GRADLE_TRUST_OPTS, "");
+            environmentVariables.put(PLACEHOLDER_TRUST_BUNDLE_MOUNT_PATH, "");
+            environmentVariables.put(PLACEHOLDER_TRUST_BUNDLE_CONFIGMAP, "");
         }
 
         if (arguments.isEnableMonitor()) {
